6281d2f2403e7f9b7648c8dfa07a942ced69d56d
Daniel Oaks <danneh@danneh.net>
2013-12-21 15:41:01 +1000

    Gravity kinda sorta a little better I guess

--- main.lua
+++ main.lua
@@ -15,6 +15,11 @@ function pix(x, y)
     return (math.floor(x) + 0.5), (math.floor(y) + 0.5)
 end
 
+function clip(num, min, max)
+    -- clip num to min and max
+    return math.max(math.min(num, max), min)
+end
+
 
 iteration = 0
 
@@ -24,13 +29,25 @@ function init_bodies()
 
     -- initialize our bodies
     bodies = {}
-    max_bodies = 5   -- how many bodies we want
+    max_bodies = 34   -- how many bodies we want
 
     -- make warmup time faster, try to minimise function calls
     local screen_width = love.graphics.getWidth()
     local screen_height = love.graphics.getHeight()
 
     -- create our bodies
+    for i = 1, max_bodies do
+        bodies[i] = {  -- moons
+            pos = vector(math.random(20, screen_width - 20), 
+                         math.random(20, screen_height - 20)),
+            vel = vector(math.random(-2, 2),
+                         math.random(-2, 2)),
+            weight = math.floor(math.random(6, 20)),
+            color = {math.floor(math.random(10, 255)),
+                     math.floor(math.random(10, 255)),
+                     math.floor(math.random(10, 255)), 255},
+        }
+    end
     bodies[1] = {  -- planet
         pos = vector(screen_width / 2, screen_height / 2),
         vel = vector(math.random(-2, 2),
@@ -40,18 +57,6 @@ function init_bodies()
                  math.floor(math.random(10, 255)),
                  math.floor(math.random(10, 255)), 255},
     }
-    for i = 2, max_bodies do
-        bodies[i] = {  -- moons
-            pos = vector(math.random(20, screen_width - 20), 
-                         math.random(20, screen_height - 20)),
-            vel = vector(math.random(-2, 2),
-                         math.random(-2, 2)),
-            weight = math.floor(math.random(2, 20)),
-            color = {math.floor(math.random(10, 255)),
-                     math.floor(math.random(10, 255)),
-                     math.floor(math.random(10, 255)), 255},
-        }
-    end
 end
 
 
@@ -71,8 +76,10 @@ end
 function love.draw()
     -- keep giant body centred
     love.graphics.push()
-    love.graphics.translate((love.graphics.getWidth() / 2 - bodies[1].pos.x) - bodies[1].vel.x * 2,
-                            (love.graphics.getHeight() / 2 - bodies[1].pos.y) - bodies[1].vel.y * 2)
+    -- love.graphics.translate((love.graphics.getWidth() / 2 - bodies[1].pos.x) - bodies[1].vel.x,
+    --                         (love.graphics.getHeight() / 2 - bodies[1].pos.y) - bodies[1].vel.y)
+    -- love.graphics.translate((love.graphics.getWidth() / 2 - bodies[1].pos.x),
+    --                         (love.graphics.getHeight() / 2 - bodies[1].pos.y))
 
     -- drawings!
     for i = 1, max_bodies do
@@ -86,16 +93,33 @@ function love.draw()
         -- gravity
         -- NOTE: I suck at gravity
         for j = 1, max_bodies do
-            bodies[i].vel = bodies[i].vel + (bodies[j].pos - bodies[i].pos) * 0.0004 * ((1000 - bodies[i].pos:dist(bodies[j].pos)) * 0.0005) * math.abs(bodies[j].weight * 2 / bodies[i].weight)
+            -- NOTE: lots and lots of failed attempts, dissapointing for my old math teacher
+            vel_mod = 0.000001 * (201 - clip(bodies[i].pos:dist(bodies[j].pos), 1, 200))
+            bodies[i].vel = bodies[i].vel + (bodies[j].pos - bodies[i].pos) * bodies[j].weight * vel_mod --* (400.0 - clip(bodies[i].pos:dist2(bodies[j].pos) / 10000, 20, 40))
+            -- g_force = clip((bodies[i].weight * bodies[j].weight) / bodies[i].pos:dist(bodies[j].pos) * 10, 0, 200)
+            -- print(g_force)
+            -- bodies[i].vel = bodies[i].vel + (bodies[j].pos - bodies[i].pos) * 0.0004
+            -- bodies[i].vel = bodies[i].vel + (math.abs(10000 - clip(bodies[j].pos - bodies[i].pos), 0, 10000) / 10000)
+            -- print(bodies[i].pos:dist2(bodies[j].pos))
+            -- local reach = bodies[j].weight
+            -- gravity = (bodies[j].pos - bodies[i].pos) * (reach * 10 - clip(bodies[i].pos:dist2(bodies[j].pos) / reach, 0, reach * 10))
+            -- bodies[i].vel = bodies[i].vel + (0.0002 * gravity)
+            -- bodies[i].vel = bodies[i].vel + ((bodies[j].pos - bodies[i].pos) * 0.0004) * ((1000 - bodies[i].pos:dist(bodies[j].pos)) * 0.0005) * math.abs(bodies[j].weight * 2 / bodies[i].weight)
+            -- bodies[i].vel = bodies[i].vel + ((bodies[j].pos - bodies[i].pos) * 0.0004 * (100 - clip(bodies[i].pos:dist2(bodies[j].pos) * 0.00001), 0, 100))
         end
     end
 
     love.graphics.pop()
+
+    time_offset = os.difftime(os.time(), start_time)
+
+    -- print iteration and seconds remaining
     love.graphics.setColor(200,200,200)
-    love.graphics.print(tostring(iteration), 10, 10)
+    love.graphics.print(iteration, 10, 10)
+    love.graphics.print(6 - math.floor(time_offset), 10, 25)
 
     -- refresh!
-    if os.difftime(os.time(), start_time) > 3 then
+    if time_offset > 5.9 then
         init_bodies()
         start_time = os.time()
     end
