ccfaa86acc6dca14b2000ff23891ec1c0e228ba4
Daniel Oaks <daniel@danieloaks.net>
2015-06-09 21:59:35 +1000

    Pixelate assembles shader on the fly now

--- shine/pixelate.lua
+++ shine/pixelate.lua
@@ -23,34 +23,32 @@ SOFTWARE.
 ]]--
 
 local function build_shader(add_original, samples)
-	return love.graphics.newShader[[
+	local code = {[[
 	extern float pixel_size;
 
 	vec4 effect(vec4 vcolor, Image texture, vec2 texture_coords, vec2 pixel_coords)
 	{
-		vec4 original_rgb = Texel(texture, texture_coords);
+	]]}
 
-		float r = original_rgb.r;
-		float g = original_rgb.g;
-		float b = original_rgb.b;
-		int count = 1;
+	if (add_original) then
+		code[#code+1] = [[
+			vec4 original_rgb = Texel(texture, texture_coords);
 
-		r = 0;
-		g = 0;
-		b = 0;
-		count = 0;
+			float r = original_rgb.r;
+			float g = original_rgb.g;
+			float b = original_rgb.b;
+			int count = 1;
+		]]
+	else
+		code[#code+1] = [[
+			float r = 0;
+			float g = 0;
+			float b = 0;
+			int count = 0;
+		]]
+	end
 
-		// pixelate, collecting 'samples' number of samples for each 'pixel'
-		/*float pix_x = floor((pixel_coords.x * love_ScreenSize.x) / pixel_size);
-		float start_of_pixel_x = (pix_x + 0.2) * float(pixel_size) / float(love_ScreenSize.x);
-		float end_of_pixel_x = (pix_x + 0.8) * float(pixel_size) / float(love_ScreenSize.x);
-		float mid_of_pixel_x = (start_of_pixel_x + end_of_pixel_x) / 2.0;
-
-		float pix_y = floor((pixel_coords.y * love_ScreenSize.y) / pixel_size);
-		float start_of_pixel_y = (pix_y + 0.2) * float(pixel_size) / float(love_ScreenSize.y);
-		float end_of_pixel_y = (pix_y + 0.8) * float(pixel_size) / float(love_ScreenSize.y);
-		float mid_of_pixel_y = (start_of_pixel_y + end_of_pixel_y) / 2.0;*/
-
+	code[#code+1] = [[
 		float pix_x = floor(float(pixel_coords.x) / pixel_size);
 		float start_of_pixel_x = (pix_x + 0.2) * float(pixel_size) / float(love_ScreenSize.x);
 		float end_of_pixel_x = (pix_x + 0.8) * float(pixel_size) / float(love_ScreenSize.x);
@@ -65,49 +63,73 @@ local function build_shader(add_original, samples)
 		// XXX - can r/g/b overflow?
 		vec4 working_pix;
 		vec2 working_coords;
+	]]
 
-		working_coords.x = start_of_pixel_x;
-		working_coords.y = start_of_pixel_y;
-		working_pix = Texel(texture, working_coords);
-		r += working_pix.r; g += working_pix.g; b += working_pix.b;
-		count++;
+	if (samples >= 1) then
+		code[#code+1] = [[
+			working_coords.x = mid_of_pixel_x;
+			working_coords.y = mid_of_pixel_y;
+			working_pix = Texel(texture, working_coords);
+			r += working_pix.r; g += working_pix.g; b += working_pix.b;
+			count++;
+		]]
+	end
+	if (samples >= 2) then
+		code[#code+1] = [[
+			working_coords.x = start_of_pixel_x;
+			working_coords.y = start_of_pixel_y;
+			working_pix = Texel(texture, working_coords);
+			r += working_pix.r; g += working_pix.g; b += working_pix.b;
+			count++;
+		]]
+	end
+	if (samples >= 3) then
+		code[#code+1] = [[
+			working_coords.x = end_of_pixel_x;
+			working_coords.y = end_of_pixel_y;
+			working_pix = Texel(texture, working_coords);
+			r += working_pix.r; g += working_pix.g; b += working_pix.b;
+			count++;
+		]]
+	end
+	if (samples >= 4) then
+		code[#code+1] = [[
+			working_coords.x = start_of_pixel_x;
+			working_coords.y = mid_of_pixel_y;
+			working_pix = Texel(texture, working_coords);
+			r += working_pix.r; g += working_pix.g; b += working_pix.b;
+			count++;
+		]]
+	end
+	if (samples >= 5) then
+		code[#code+1] = [[
+			working_coords.x = end_of_pixel_x;
+			working_coords.y = start_of_pixel_y;
+			working_pix = Texel(texture, working_coords);
+			r += working_pix.r; g += working_pix.g; b += working_pix.b;
+			count++;
+		]]
+	end
+	if (samples >= 6) then
+		code[#code+1] = [[
+			working_coords.x = start_of_pixel_x;
+			working_coords.y = end_of_pixel_y;
+			working_pix = Texel(texture, working_coords);
+			r += working_pix.r; g += working_pix.g; b += working_pix.b;
+			count++;
+		]]
+	end
+	if (samples >= 7) then
+		code[#code+1] = [[
+			working_coords.x = mid_of_pixel_x;
+			working_coords.y = start_of_pixel_y;
+			working_pix = Texel(texture, working_coords);
+			r += working_pix.r; g += working_pix.g; b += working_pix.b;
+			count++;
+		]]
+	end
 
-		working_coords.x = start_of_pixel_x;
-		working_coords.y = mid_of_pixel_y;
-		working_pix = Texel(texture, working_coords);
-		r += working_pix.r; g += working_pix.g; b += working_pix.b;
-		count++;
-
-		working_coords.x = start_of_pixel_x;
-		working_coords.y = end_of_pixel_y;
-		working_pix = Texel(texture, working_coords);
-		r += working_pix.r; g += working_pix.g; b += working_pix.b;
-		count++;
-
-		working_coords.x = mid_of_pixel_x;
-		working_coords.y = start_of_pixel_y;
-		working_pix = Texel(texture, working_coords);
-		r += working_pix.r; g += working_pix.g; b += working_pix.b;
-		count++;
-
-		working_coords.x = end_of_pixel_x;
-		working_coords.y = start_of_pixel_y;
-		working_pix = Texel(texture, working_coords);
-		r += working_pix.r; g += working_pix.g; b += working_pix.b;
-		count++;
-
-		working_coords.x = mid_of_pixel_x;
-		working_coords.y = mid_of_pixel_y;
-		working_pix = Texel(texture, working_coords);
-		r += working_pix.r; g += working_pix.g; b += working_pix.b;
-		count++;
-
-		working_coords.x = end_of_pixel_x;
-		working_coords.y = end_of_pixel_y;
-		working_pix = Texel(texture, working_coords);
-		r += working_pix.r; g += working_pix.g; b += working_pix.b;
-		count++;
-
+	code[#code+1] = [[
 		// average
 		r /= count; g /= count; b /= count;
 
@@ -121,6 +143,8 @@ local function build_shader(add_original, samples)
 		return rgb_out;
 	}
 	]]
+
+	return love.graphics.newShader(table.concat(code))
 end
 
 return {
@@ -128,14 +152,14 @@ requires = {'canvas', 'shader'},
 description = "Pixelating input",
 
 new = function(self)
-	self.pixel_size = 3
-	self.add_original = true
-	self.samples = 8
+	self._pixel_size = 3
+	self._add_original = true
+	self._samples = 7
 
 	self.canvas = love.graphics.newCanvas()
-	self.shader = build_shader(self.add_original, self.samples)
+	self.shader = build_shader(self._add_original, self._samples)
 
-	self.shader:send("pixel_size", self.pixel_size)
+	self.shader:send("pixel_size", self._pixel_size)
 end,
 
 draw = function(self, func)
@@ -145,8 +169,18 @@ end,
 set = function(self, key, value)
 	if key == "pixel_size" then
 		assert(type(value) == "number")
-		self.pixel_size = value
+		self._pixel_size = value
 		self.shader:send("pixel_size", value)
+	elseif key == "samples" then
+		assert(type(value) == "number")
+		self._samples = value
+		self.shader = build_shader(self._add_original, self._samples)
+		self.shader:send("pixel_size", self._pixel_size)
+	elseif key == "add_original" then
+		assert(type(value) == "bool")
+		self._add_original = value
+		self.shader = build_shader(self._add_original, self._samples)
+		self.shader:send("pixel_size", self._pixel_size)
 	else
 		error("Unknown property: " .. tostring(key))
 	end
